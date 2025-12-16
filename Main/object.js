79aaba362b6e3377463bf575c595624f0147981e
Main-April <main.april.yt@gmail.com>
2025-12-16 16:23:16 +0100

    Fix iterate function to check property enumeration

--- Main/object.js
+++ Main/object.js
@@ -21,7 +21,7 @@ class NObject extends Object{
 }
 
 function iterate(a,c){
-    if(a.iterable) if(type(a)=="array"||type(a)=="string") for(i=0;i<a.length;i++){c(a[i])} 
+    if(a..propertyIsEnumerable()) if(type(a)=="array"||type(a)=="string") for(i=0;i<a.length;i++){c(a[i])} 
     else a.forEach((i)=>{c(i)});
     else throw new TypeError("[Nanotypes.js]:object Iterable type is require in iterate function")
 }
